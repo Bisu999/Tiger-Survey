50fe6804f901560f9e4e333f0ada635acc897c71
Biswajit debata <100460857+Bisu999@users.noreply.github.com>
2022-09-15 23:04:19 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import CreateSurvey from './component/create-survey';
+import { Button } from 'reactstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"  /></header>
+    <Router>
+     
+        <Routes>
+          <Route path="/create" ></Route>
+          <Route path="/take">Take Survey</Route>
+          <Route path="/">
+            
+              <Link to="/create"> <Button className="survey-main-btn">Create Survey</Button></Link> 
+              <Link to="/take"> <Button className="survey-main-btn">Take Survey</Button></Link>
+        
+          </Route>
+        </Routes>
+      
+    </Router> 
     </div>
   );
 }
